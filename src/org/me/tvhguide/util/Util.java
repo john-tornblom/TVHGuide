0928d3dafcfd17f53ed99eca49f3f0b8e53c7c44
Ton van der Poel <ton.van.der.poel@gmail.com>
2011-10-10 20:38:34 +0200

    Updated Layout - Cleaned up

---
+++ src/org/me/tvhguide/util/Util.java
@@ -0,0 +1,59 @@
+package org.me.tvhguide.util;
+
+import java.util.Date;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+
+public class Util {
+
+	public static String getDate(Context ctx, Date start, Date stop) {
+		String retVal = "";
+
+		if (isToday(start)) 
+			retVal = "Today";
+		else if (isTomorrow(start))
+			retVal = "Tomorrow";
+		else
+			retVal = DateFormat.getLongDateFormat(ctx).format(start);
+		
+		retVal += "   " + DateFormat.getTimeFormat(ctx).format(start)
+        	+ " - "
+        	+ DateFormat.getTimeFormat(ctx).format(stop);
+		
+		return retVal;
+	}
+	
+	public static String getDate(Context ctx, Date start) {
+		String retVal = "";
+
+		if (isToday(start)) 
+			retVal = "Today";
+		else if (isTomorrow(start))
+			retVal = "Tomorrow";
+		else
+			retVal = DateFormat.getMediumDateFormat(ctx).format(start);
+				
+		return retVal;
+	}
+
+	static Boolean isToday(Date start) {
+		Date compDate = new Date();
+		if (compDate.getMonth() == start.getMonth())
+			if (compDate.getDate() == start.getDate())
+				return true;
+
+		return false;
+	}
+	
+	static Boolean isTomorrow(Date start) {
+		Date compDate = new Date();
+		compDate.setTime(compDate.getTime() + (1 * 24 * 60 * 60 * 1000));
+		if (compDate.getMonth() == start.getMonth())
+			if (compDate.getDate() == start.getDate())
+				return true;
+
+		return false;
+	}
+	
+}
