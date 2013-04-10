b9e7c1115d534b0908c83cfdff2dc75c5c807a3b
mike <mike@saruman.(none)>
2013-04-10 17:31:12 +0200

    added missing files

--- src/org/tvheadend/tvhguide/EPGNowListActivity.java
+++ src/org/tvheadend/tvhguide/EPGNowListActivity.java
@@ -76,7 +76,7 @@ public class EPGNowListActivity extends FragmentActivity implements
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.epgnow_list_widget, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
 
